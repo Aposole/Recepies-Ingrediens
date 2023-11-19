ca9f3801fe427c90e53e4f2e7ebef3df9d32a966
Chouchene Med Nour <mednourchouchene@gmail.com>
2023-11-19 11:09:20 +0100

    First commit

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipeService } from './recipes/recipe.service';
